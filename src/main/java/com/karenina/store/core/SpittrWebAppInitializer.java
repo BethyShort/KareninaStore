24d5202cb3afebca01ad9db718db948531b1194b
eli <elisabeth_short@yahoo.es>
2018-01-14 23:33:19 +0100

    Version inicial

---
+++ src/main/java/com/karenina/store/core/SpittrWebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.karenina.store.core;
+
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[]{RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[]{WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		//asignar DispathcerServlet a "/."
+		return new String[] {"/"};
+	}
+
+}
